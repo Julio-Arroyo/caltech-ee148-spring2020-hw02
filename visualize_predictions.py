b70d1e32b8db20800768a877722397f56c43dbeb
Julio-Arroyo <jarroyoi@caltech.edu>
2022-04-09 19:28:33 -0700

    Add feature to visualize predictions and ground truth simultaneously

--- visualize_predictions.py
+++ visualize_predictions.py
@@ -7,18 +7,22 @@ import json
 if __name__ == '__main__':
     algo = 'find_red'
     # preds_path = f'./data/hw01_preds/{algo}'
-    annotators = 'mturk'  #students_2021, students_2020, mturk
+    annotators = 'students_2021'  #students_2021, students_2020, mturk
     preds_path = f'data/ground_truth'
     data_path = '/Users/jarroyo/OneDrive - California Institute of Technology/Courses/2022Spring/CS148/RedLights2011_Medium'
 
     # f = open(f'{preds_path}/preds.json')
     f = open(f'{preds_path}/formatted_annotations_{annotators}.json')
     img_2_bboxes = json.load(f)
-
+    f_pred = open(f'preds/preds_train_find_red.json')
+    pred_bboxes = json.load(f_pred)
     fig, ax = plt.subplots()
 
     for img_name in img_2_bboxes:
+        if img_name not in pred_bboxes:  # some images are saved for test set
+            continue
         bboxes = img_2_bboxes[img_name]
+        bboxes_pred = pred_bboxes[img_name]
         img = Image.open(f'{data_path}/{img_name}')
         ax.imshow(img)
 
@@ -32,12 +36,22 @@ if __name__ == '__main__':
                                      linewidth=2,
                                      edgecolor='m',
                                      facecolor='none')
-
             # Add the patch to the Axes
             ax.add_patch(rect)
+
+        for bbox in bboxes_pred:
+            width = abs(bbox[2] - bbox[0])
+            height = abs(bbox[3] - bbox[1])
+            rect_pred = patches.Rectangle((bbox[0], bbox[1]),
+                                     height,
+                                     width,
+                                     linewidth=2,
+                                     edgecolor='c',
+                                     facecolor='none')
+            ax.add_patch(rect_pred)
         img_id = img_name.strip('.jpg')
         # plt.savefig(f'{preds_path}/{img_id}_pred.jpg')
-        plt.savefig(f'{preds_path}/{annotators}/{img_id}_gt.jpg')
+        plt.savefig(f'preds/images/{annotators}/{img_id}_gt.jpg')
 
         plt.cla()
     f.close()
